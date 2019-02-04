c7fa55a700dfaa6e53032b4de9e4409e6827d2be
raehelen <41242991+raehelen@users.noreply.github.com>
2019-02-04 16:33:21 +0000

    Update _main.js

--- assets/js/_main.js
+++ assets/js/_main.js
@@ -107,18 +107,29 @@ $(document).ready(function() {
 });
 
   //Products Search
-   
-  $('.selectpicker').selectpicker();
-   
-var dropDownValue = document.getElementById("dropDown");
 
-dropDownValue.onchange = function()
-{
-if(this.selectedIndex !== 0)
-{
-    window.location.hash = this.value;
+/* When the user clicks on the button,
+toggle between hiding and showing the dropdown content */
+function myFunction() {
+  document.getElementById("myDropdown").classList.toggle("show");
 }
-};
+
+function filterFunction() {
+  var input, filter, ul, li, a, i;
+  input = document.getElementById("myInput");
+  filter = input.value.toUpperCase();
+  div = document.getElementById("myDropdown");
+  a = div.getElementsByTagName("a");
+  for (i = 0; i < a.length; i++) {
+    txtValue = a[i].textContent || a[i].innerText;
+    if (txtValue.toUpperCase().indexOf(filter) > -1) {
+      a[i].style.display = "";
+    } else {
+      a[i].style.display = "none";
+    }
+  }
+}
+
    
    
 
