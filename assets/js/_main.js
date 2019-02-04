1fc80ad33529d67c72f1d5e91c69433977c53a40
raehelen <41242991+raehelen@users.noreply.github.com>
2019-02-04 23:39:44 +0000

    Update _main.js

--- assets/js/_main.js
+++ assets/js/_main.js
@@ -106,6 +106,21 @@ $(document).ready(function() {
   });
 });
 
+//product search
+
+$(".dropdown dt a").click(function() {
+    $(".dropdown dd ul").toggle();
+});
+$(".dropdown dd ul li a").click(function() {
+    var text = $(this).html();
+    $(".dropdown dt a span").html(text);
+    $(".dropdown dd ul").hide();
+});
+$(document).bind('click', function(e) {
+    var $clicked = $(e.target);
+    if (! $clicked.parents().hasClass("dropdown"))
+        $(".dropdown dd ul").hide();
+});
 
    
    
