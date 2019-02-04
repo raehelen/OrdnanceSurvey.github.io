e3ef177ca507e9be988943a123d5e520c0520f0a
raehelen <41242991+raehelen@users.noreply.github.com>
2019-02-04 08:43:25 +0000

    Update _main.js

--- assets/js/_main.js
+++ assets/js/_main.js
@@ -35,7 +35,7 @@ $(document).ready(function() {
   };
    
   $(document).ready(function () {
-     $('#select-anchor').change( function () {
+     $('#dropDown').change( function () {
          var targetPosition = $($(this).val()).offset().top;
      $('html,body').animate({ scrollTop: targetPosition}, 'slow');
      });
